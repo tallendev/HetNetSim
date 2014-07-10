8c62e56e48e5e1fc58b72f2f0f5fe074886e9331
Matthew and Tyler <hetnetsim@nlab.cs.clemson.edu>
2014-07-10 17:02:55 -0400

    Color option disabled for non-colorable selections. Networks now resizable.

---
+++ test/test.cpp
@@ -0,0 +1,22 @@
+#include "Solver.h"
+#include <iostream>
+
+int main(void)
+{
+    std::io
+    LinearProgram* pTestProblem = new LinearProgram("5 4 3");
+    pTestProblem->addLeqConstraint("2 3 1 5");
+    pTestProblem->addLeqConstraint("4 1 2 11");
+    pTestProblem->addLeqConstraint("3 4 2 8");
+    LPSolution* answer = Solver::getInstance().solve(pTestProblem);
+    std::cout << "answer error code = " << answer->getErrorCode() << std::endl;
+    if (answer->getErrorCode() == 0) {
+    std::cout << "z value: " << answer->getZValue() << std::endl;
+    double* answervals = answer->getOptimalValues();
+    std::cout << "answer values: " << answervals[0] << std::endl; 
+    std::cout << "answer values: " << answervals[1] << std::endl; 
+    std::cout << "answer values: " << answervals[2] << std::endl; 
+    }
+    delete pTestProblem;
+    return 0;
+}
--- www/simulation.js
+++ www/simulation.js
@@ -2,57 +2,141 @@
  *
  */
 
-var canvas = d3.select("body")
+var w;
+var h;
+var border;
+var borderColor;
+
+var canvas;
+
+
+var buttonFuncs;
+var colorArray;
+var color;
+var shape;
+
+function main()
+{
+    w = 960;
+    h = 640;
+    border = 5;
+    borderColor = "gray";
+
+    canvas = d3.select("body")
                .append("svg")
-               .attr("width", 500)
-               .attr("height", 500);
+               .attr("width", w)
+               .attr("height", h);
 
-var colorArray = ["red", "green", "blue"];
-var color = colorArray[0];
+   borderPath = canvas.append("rect")
+                           .attr("x", 0)
+                           .attr("y", 0)
+                           .attr("width", w)
+                           .attr("height", h)
+                           .style("stroke", borderColor)
+                           .style("fill", "none")
+                           .style("stroke-width", border);
 
-function genCircle(svg, cx, cy, radius, newColor)
-{
-    console.log("Circle generated");
-    svg.append("circle")
-          .attr("cx", cx)
-          .attr("cy", cy)
-          .attr("r", radius)
-          .style("fill", newColor);
-}
+    buttonFuncs = {'circle' : genCircle, 
+                   'triangle' : genTriangle}//, 
+                   //'resize' : setResizable};
+    colorArray = ["red", "green", "blue"];
+    color = colorArray[0];
+    shape = "circle";
+    $('input[value="circle"]').prop('checked', true);
+    $('input[value="red"]').prop('checked', true);
 
-// TODO
-function genTriangle(svg, cx, cy, radius, newColor)
-{
-    console.log("Circle generated");
-    svg.append("circle")
-          .attr("cx", cx)
-          .attr("cy", cy)
-          .attr("r", radius)
-          .style("fill", newColor);
-}
 
-var buttonFuncs = {'circle' : genCircle};
-
-function colorChange()
-{
-    color = $('input[name=color]:checked').val();
-}
-
-function shapeChange()
-{
-    var radio = $('input[name=shape]:checked').val();
-    console.log(radio);
-    var clickFunc = buttonFuncs[radio];
-    console.log("listener changed.");
     canvas.on("click", function()
     {
         console.log("clicked");
-        clickFunc(canvas, d3.mouse(this)[0], d3.mouse(this)[1], 
-                  50, color);
+        console.log("x: " + d3.mouse(this)[0] + " y: " + d3.mouse(this)[1]);
+        drawFunc = buttonFuncs[shape];
+        if (drawFunc != null)
+        {
+            drawFunc(canvas, d3.mouse(this)[0], d3.mouse(this)[1], 50, color);
+        }
     });
 }
 
 
+function genCircle(svg, cx, cy, radius, newColor)
+{
+    console.log("Circle generated");
+    var theColor = d3.rgb(newColor);
+    svg.append("circle")
+          .attr("cx", cx)
+          .attr("cy", cy)
+          .attr("r", radius)
+          .attr("stroke", theColor.darker(.9))
+          .attr("stroke-width", radius / 10)
+          //value for opacity subject to change. we may want to use light colors
+          //exclusively in the final version, the dark colors probably aren't that
+          //much easier to see.
+          .attr("opacity", .5)
+          .style("fill", theColor);
+}
+
+function genTriangle(svg, xCoord, yCoord, size, newColor)
+{
+    console.log("Triangle generated");
+    svg.append("path")
+          .attr("class", "point")
+          .style("fill", "grey")
+          .attr("d", d3.svg.symbol().type("triangle-up"))
+          .attr("size", size)
+          .attr("transform", function(x, y) {
+              return "translate(" + xCoord + "," + yCoord + ")"; 
+              });
+}
+
+
+function colorChange()
+{
+    color = $('input[name=color]:checked').val();
+    console.log(color);
+}
+
+function shapeChange()
+{
+    shape = $('input[name=shape]:checked').val();
+    if (shape === "circle")
+    {
+        $('#color').show();
+    }
+    else
+    {
+        if (shape === "resize")
+        {
+            setResizable();
+        }
+        else
+        {
+            unsetResizable();
+        }
+        $('#color').hide();
+    }
+    console.log(shape);
+}
+
+function setResizable()
+{
+    console.log("setResizable!");
+    d3.selectAll("circle").call(d3.behavior.drag().on("drag", resize));
+}
+
+function unsetResizable()
+{
+    console.log("unsetResizable!");
+    d3.selectAll("circle").call(d3.behavior.drag().on("drag"));
+}
+
+function resize()
+{
+    console.log("resize");
+    thisCircle = d3.select(this);
+    var mouse = d3.mouse(this);
+    thisCircle.attr("r", Math.abs(thisCircle.attr("cx") - mouse[0]));
+}
 
 $(document).ready(function() {
     $('form').submit(function(event)
@@ -74,3 +158,5 @@ $(document).ready(function() {
         event.preventDefault();
     });
 });
+
+main();
